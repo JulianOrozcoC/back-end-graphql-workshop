fc884263a671f3d407cd50108fca480a603d291e
Ricardo R.S <ricardors94@hotmail.com>
2018-10-19 15:25:11 -0500

    Workshop initial state

--- graphql/resolvers/post.resolver.js
+++ graphql/resolvers/post.resolver.js
@@ -9,11 +9,6 @@ const posts = async (parent, args, { Post }) => {
   return results.map(objIdToString);
 };
 
-const comments = async ({ _id }, args, { Comment }) => {
-  const results = await Comment.find({ postId: _id, active: true });
-  return results.map(objIdToString);
-};
-
 const createPost = async (parent, args, { Post }) => {
   try {
     return await Post.create(args.input);
@@ -22,29 +17,7 @@ const createPost = async (parent, args, { Post }) => {
   }
 };
 
-const updatePost = async (parent, args, { Post }) => {
-  try {
-    return await Post.findByIdAndUpdate(args._id, args.input, {
-      new: true,
-    });
-  } catch (error) {
-    return error;
-  }
-};
-
-const deletePost = async (parent, args, { Post }) => {
-  try {
-    const post = await Post.findById(args._id);
-    post.active = false;
-    post.save();
-    return post;
-  } catch (error) {
-    return error;
-  }
-};
-
 export default {
   Query: { post, posts },
-  Mutation: { createPost, updatePost, deletePost },
-  Post: { comments },
+  Mutation: { createPost },
 };
